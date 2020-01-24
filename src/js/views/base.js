505a2ae807bf7544e04e7ddeae6b6309751265e2
Dre <coledre1100@gmail.com>
2020-01-24 17:02:51 -0500

    Change the query method to find players by name

---
+++ src/js/views/base.js
@@ -0,0 +1,9 @@
+//CREATING AN OBJ THAT CONTAINS ALL THE DOM ELEMENTS WE NEED FROM OUR APP
+export const elements = {
+    player0SearchQuery: document.querySelector('#player_0_search_result'),
+    player1SearchQuery: document.querySelector('#player_1_search_result')
+}
+
+export const elementStrings = {
+    loader: 'loader'
+}
---
+++ src/js/views/searchView.js
@@ -0,0 +1,3 @@
+import {elements} from './base'
+
+export const getInput = () => elements.player0SearchQuery.value;
